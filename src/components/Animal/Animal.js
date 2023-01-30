82310e717f722dbf637f0cc04d54dd19b765817a
Kenni Woodard <kennikristine@gmail.com>
2023-01-30 09:29:06 -0800

    animal imgages, names, and says are displaying to main, no css

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,9 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <div>
+      <img src={`/animals/${props.type}.svg`} />
+      <p className="name">{props.name}</p>
+      <p>{props.says}</p>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import './Main.css';
 import background from '../../background.png';
+import { animals } from '../../data.js';
+import Animal from '../Animal/Animal.js';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }}></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }}>
+      {animals.map((prop) => (
+        <Animal key={prop.name} {...prop} />
+      ))}
+    </main>
+  );
 }
